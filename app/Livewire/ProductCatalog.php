4f8d79bb2f8066cf1a686295b2a9396f46e11d26
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-14 10:38:15 +0700

    CodeMind Collections

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace App\Livewire;
 
+use App\Data\ProductCollectionData;
 use App\Data\ProductData;
 use App\Models\Product;
+use App\Models\Tag;
 use Livewire\Component;
 
 class ProductCatalog extends Component
@@ -12,11 +14,13 @@ class ProductCatalog extends Component
     public function render()
     {
 
+        $collection_result = Tag::query()->withType('collection')->withCount('products')->get();
+        
         $result = Product::paginate(9); // ORM / Database Query
         $products = ProductData::collect($result);
        
-      
+        $collections = ProductCollectionData::collect($collection_result);
 
-        return view('livewire.product-catalog', compact('products'));
+        return view('livewire.product-catalog', compact('products', 'collections'));
     }
 }
