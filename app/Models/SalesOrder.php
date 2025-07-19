757656e9758587bdddf177b2230d722c1c9fb6fc
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-19 12:07:15 +0700

    CodeMind Sales Order Model

--- app/Models/SalesOrder.php
+++ app/Models/SalesOrder.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use App\States\SalesOrder\SalesOrderState;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\ModelStates\HasStates;
 
 class SalesOrder extends Model
 {
 
+    use HasStates;
+    
+
     protected $with = ['items'];
 
     protected $casts = [
+        'status' => SalesOrderState::class,
         'payment_payload' =>'json'
     ];
 
--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -7,6 +7,7 @@ use App\Data\CheckoutData;
 use App\Data\SalesOrderData;
 use App\Models\Product;
 use App\Models\SalesOrder;
+use App\States\SalesOrder\Pending;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -21,7 +22,7 @@ class CheckoutService {
             $items = collect([]);
             $sales_order = SalesOrder::query()->create([
                 'trx_id' => "TRX-{$date}-{$random}",
-                'status' => 'pending',
+                'status' => Pending::class,
                 'customer_full_name' => $checkout_data->customer->full_name,
                 'customer_email' => $checkout_data->customer->email,
                 'customer_phone' => $checkout_data->customer->phone,
